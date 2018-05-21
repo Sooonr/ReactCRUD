f1273cad0cbf4047578d17700ff15463965e992c
Aiden <aidenone.lucas@gmail.com>
2018-05-21 16:22:08 +0200

    Fonction update

--- server.js
+++ server.js
@@ -35,6 +35,35 @@ app.listen(port, function() {
  console.log(`api running on port ${port}`);
 });
 
+router.route('/quote/update/:id')
+ //retrieve a quote from the database by id
+ .get(function(req, res) {
+     const id = req.originalUrl.split('/')[3];
+     //looks at our Quote Schema
+     Quote.findById(id, function(err, quotes) {
+       if (err)
+         res.send(err);
+         //responds with a json object of our database quotes.
+         res.json(quotes)
+    });
+ })
+
+ .post(function(req, res) {
+     const id = req.originalUrl.split('/')[4];
+     //body parser lets us use the req.body
+     Quote.findById(id, function(err, quote) {
+       if (err)
+         res.send(err);
+         quote.name = req.body.name;
+         quote.quote = req.body.quote;
+         quote.save(function(err) {
+           if (err)
+             res.send(err);
+             res.json({ message: 'Quote successfully updated!' });
+        });
+     });
+ });
+
 router.route('/quote/:id')
  //retrieve a quote from the database by id
  .get(function(req, res) {
---
+++ src/components/UpdateQuote/index.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import { StyleSheet, css } from 'aphrodite';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+class UpdateQuote extends Component {
+
+  state = {
+    data: {},
+    loading: true,
+   };
+
+  componentDidMount = () => {
+    this.loadQuotesFromServer();
+  }
+
+   loadQuotesFromServer = () => {
+     const idQuote = this.props.location.pathname.split('/')[3]
+     axios.get('http://localhost:3001/api/quote/' + idQuote)
+     .then(res => {
+       this.setState({ data: res.data, loading: false });
+     })
+   }
+
+   updateContent = ({ target: {value, name} }) => {
+       if (name === 'name') {
+         this.setState({
+           quoteName: value
+         });
+       } else if (name === 'quote') {
+         this.setState({
+           quoteContent: value
+         });
+       }
+    }
+
+    updateQuoteOnServer = (e) => {
+     e.preventDefault();
+     const idQuote = this.props.location.pathname.split('/')[3]
+     const {quoteName, quoteContent} = this.state;
+     axios.post('http://localhost:3001/api/quote/update/' + idQuote, {
+       name: quoteName,
+       quote: quoteContent,
+     })
+     .then(res => {
+       if (res.data.error) {
+         this.setState({ data: res.data })
+       } else {
+         this.setState({
+           data: res.data,
+           quoteName: '',
+           quoteContent: '',
+           loading: false,
+           redirect: true,
+         });
+       }
+     })
+   }
+
+  render() {
+
+    const { data } = this.state;
+
+    return (
+      <div className={css(styles.container)}>
+        <form className={css(styles.form)} onSubmit={this.updateQuoteOnServer} >
+          <input placeholder={data.name} type="text" name="name" onChange={this.updateContent}></input>
+          <input placeholder={data.quote} type="text" name="quote" onChange={this.updateContent}></input>
+          <button type="submit">Valider</button>
+          {data.message &&
+            <div>{data.message}</div>
+          }
+        </form>
+      </div>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      marginTop: 25,
+      width: '100%',
+      display: 'flex',
+      justifyContent: 'center',
+    },
+    form: {
+      display: 'flex',
+      flexDirection: 'column',
+      width: 500,
+    },
+});
+export default UpdateQuote;
